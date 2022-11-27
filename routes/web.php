1ad10c0cbab1170be4b03901c571822f6b2bb8bf
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2022-11-27 03:59:50 -0600

    comienzo del proyecto

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\HabitacionController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
